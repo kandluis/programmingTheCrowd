5d331bfbdc6b11d3b1ec5cdc2a99ec09506b8ff6
Tiffany Wu <tiffany234wu@gmail.com>
2015-04-12 16:26:55 -0400

    fixed the https issue FUCK

--- pset7-code/cs186-mturk-sandbox.js
+++ pset7-code/cs186-mturk-sandbox.js
@@ -1,26 +1,32 @@
 // Problem 2.  Random numbers
 
+function makeSecure(url) {
+return url.substring(0, 4) + "s" + url.substring(4);
+}
+
 // TO-DO:  Expand on our HTML code for the task.
 var page = createWebpageFromTemplate(
-<div>Please pick a number from 1 to 10
+<div>
+<p>Pick a random number between 1 and 10.</p>
 <textarea style="width:350px;height:30px" name="newText">..</textarea>
-<br></br>
  <input type="submit" value="Submit"></input>
 </div>);
+
+
+
 
 // TO-DO define the HIT parameters.
 var hitParams = {
         title : "Pick-a-Number",
         desc : "Pick a random number between 1 and 10.",
-        url : page,
+	url : makeSecure(page),
         height : 800,
         reward : 0.02,
-    autoApprovalDelayInSeconds : 60
     }
 
 // Create the HIT
-var hit = mturk.createHIT(hitParams)
+var hitID = mturk.createHIT(hitParams)
 print("Hit created  : "+ hit)
 
 //Report the results on the writeup
-    
+var hit = mturk.waitForHIT(hitID)
