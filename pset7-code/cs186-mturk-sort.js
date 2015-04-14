7eb4aa4116bd5c0f61da55b1d8ed3e04671139b7
Luis Perez <kandluis@yahoo.com>
2015-04-14 14:59:08 -0400

    Final submission

--- pset7-code/cs186-mturk-sort.js
+++ pset7-code/cs186-mturk-sort.js
@@ -3,24 +3,24 @@
 var pictures = ["WPLAo.jpg","Sbkem.jpg","SzBIk.jpg","ZiusC.jpg", "r53qG.jpg","RNfpa.jpg","XcGBz.jpg","YdL3d.jpg"]
 
 // TO-DO: Add "http://i.imgur.com/"    at the beginning of every picture id
-pictures = pictures.map(<YOUR CODE>)
+pictures = pictures.map(function(el){ return "http://i.imgur.com/" + e })
 
 //  Creates a webpage of two images side-by-side
 function getPicsPage(pic1, pic2) {
 
-var text=  "Which picture comes before the other chronologically? (Type 'Left' or 'Right')"
+    var text=  "Which picture comes before the other chronologically? (Type 'Left' or 'Right')"
 
-// TO-DO:   Expand on our HTML design. Do you think a different design could better?
-// Provide evidence.
-var webpage = createWebpageFromTemplate(<div>
-        <img src={pic1} width="45%" alt="Image 1"></img>
-	   <img src={pic2}  width="45%" alt="Image 2"></img>
-        <ul>
-            <li>People will vote whether to approve your work.</li>
-        </ul>
-        <textarea style="width:500px;height:50px" name="newText">{text}</textarea>
-        <input type="submit" value="Submit"></input>
-    </div>);
+    // TO-DO:   Expand on our HTML design. Do you think a different design could better?
+    // Provide evidence.
+    var webpage = createWebpageFromTemplate(<div>
+            <img src={pic1} width="45%" alt="Image 1"></img>
+    	   <img src={pic2}  width="45%" alt="Image 2"></img>
+            <ul>
+                <li>People will vote whether to approve your work.</li>
+            </ul>
+            <textarea style="width:500px;height:50px" name="newText">{text}</textarea>
+            <input type="submit" value="Submit"></input>
+        </div>);
 
 	return webpage;
 }
